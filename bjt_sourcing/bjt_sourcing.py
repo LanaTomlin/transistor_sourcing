dcfc5ef285635e6264a773aa5399fe27e59c2c08
LanaTomlin <lana.motom@gmail.com>
2021-07-15 16:18:16 -0400

    Code roughly finished and functional

--- bjt_sourcing/bjt_sourcing.py
+++ bjt_sourcing/bjt_sourcing.py
@@ -1,60 +1,58 @@
-import csv
-import datetime
-import numpy as np
 import pandas as pd
-import plotly.express as px
 import os
-import glob
+import component_sorting
 
+#Enter desired component name here
+#options: BJT
+COMPONENT = "BJT"
 
 def data_upload(data):
-    if not (data == '=' or data == '+' or data == '.' or data == None or data == 'MAIN' or data == '.MODEL' or data == '.SUBCKT' or data == 'Q1' or data == ')' or data == '('):
-        if(column !=None):
-            if(type(column) != float):
+    if (data != None and data != float) :
+        data.replace('+', ' ')
 
-                data.replace('+', '')
+        if(data.find('=') != -1):
+            parameter = data.split('=')
+            for x in parameter:
+                if(x != ''):
+                    data_upload(x)
+            data = ''
+        if(data.find('+') != -1 ):
+            data = data[1:]
+        if(data.find('\n') != -1):
+            data = data[:-1]
                
-                if(data.find('=') != -1):
-                    parameter = data.split('=')
-                    for x in parameter:
-                        x.replace('+', '')
-                        x.replace('=', '')
-                        clean_file.write(x)
-                        clean_file.write(',')
-                    data = ''
-                if(data.find('+') != -1 ):
-                    data = data[1:]
-                    print(data)
-               
-                #else:
-                    #if(data.find('+')):
-                        #   data = data[-1:]
-            if(data != '' and data != 'nan'):
-                clean_file.write(str(data))
-                clean_file.write(',')
+    if(data != '' and data != 'nan'):
+        clean_file.write(str(data))
+        clean_file.write(',')
 
-file_path = 'spice_models'
+file_path = f'{COMPONENT}/spice_models'
 for file in os.listdir(file_path):
     filename = str(file)
     file_name = filename[:-4]
-    clean_file = open(f"clean_data/{file_name}.csv", "w")
+    clean_file = open(f"{COMPONENT}/clean_data/{file_name}.csv", "w")
 
     try:
-        with open(f"spice_models/{filename}") as csvfile:
-            spice_model = pd.read_csv(csvfile, sep ="\s+", engine = 'python', comment = '*')
-
-            spice_data= spice_model.values
-
-            for row in spice_data:
-                for column in row:
-                        if(type(column) == list):
-                            for data in column:
-                                data_upload(data)
-                        else:
-                            data_upload(column)
+        with open(f"{COMPONENT}/spice_models/{filename}") as rawfile:
+            spice_model = []
+            for line in rawfile:
+                if(line.find('*') == -1):
+                    remove_space = line.split(" ")
+                    for data in remove_space:
+                        if(data != '' and data != '\n' and data != '=' and data != '+' and data != '.' and data != None and data != 'MAIN' and data != '.MODEL' and data != '.SUBCKT' and data != 'Q1' and data != ')' and data != '('):
+                            spice_model.append(data)
+                  
+            print(spice_model)
+            for row in spice_model:
+                    data_upload(row)
                     
     except ValueError:
         print("Bad file")
+
+    clean_file.close()
+
+if(COMPONENT == "BJT"):
+    component_sorting.BJT(file_name)
+
 
 
 
